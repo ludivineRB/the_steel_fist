3f6886397ee999dce4a85b8365f3783a95eb05f1
Ludivine.RB <ludivine.raby@gmail.com>
2024-11-19 13:26:15 +0100

    test

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ class Members(SQLModel, table=True):
     member_name: str = Field(index=True, nullable=False)
     email: str = Field(index=True, nullable=False)
 
-    access_card_id: int | None = Field(default=None, foreign_key="access_card_id")
+    access_card_id: int | None = Field(default=None, foreign_key="accesscards.card_id")
 
 class AccessCards(SQLModel, table=True):
     card_id: int = Field(default=None, primary_key=True, nullable=False)
@@ -18,8 +18,8 @@ class Registrations(SQLModel, table=True):
     registration_id: int | None = Field(default=None, primary_key=True)
     registration_date: datetime = Field(nullable=False)
 
-    member_id: str = Field(default=None, foreign_key="member_id")
-    course_id: str = Field(default=None, foreign_key="course_id")
+    member_id: str = Field(default=None, foreign_key="members.member_id")
+    course_id: str = Field(default=None, foreign_key="courses.course_id")
 
 class Coaches(SQLModel, table=True):
     coach_id: int | None = Field(default=None, primary_key=True)
@@ -32,4 +32,4 @@ class Courses(SQLModel, table=True):
     time_plan: datetime = Field(index=True, nullable=False)
     max_capacity: int
 
-    coach_id: int | None = Field(default=None, foreign_key="coach_id")
+    coach_id: int | None = Field(default=None, foreign_key="coaches.coach_id")
