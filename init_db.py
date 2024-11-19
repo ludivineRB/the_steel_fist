a53d6373cbd7be4ce59248a3d5e3313d64d0b344
Ludivine.RB <ludivine.raby@gmail.com>
2024-11-19 12:23:08 +0100

    initialize the db

--- init_db.py
+++ init_db.py
@@ -1,9 +1,10 @@
 from sqlmodel import SQLModel, create_engine
-import models
+import model
 
 #creation of the Engine
 sqlite_file_name = "steel_fist.db"
 sqlite_url=f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=True)
+
 SQLModel.metadata.create_all(engine)
